2f03380d7d9db31b0e40abb8501fd15fb4bf0a10
Timothy A Schutt <taschutt@syr.edu>
2016-06-16 11:52:49 -0400

    Comments are good.

--- build_installer.sh
+++ build_installer.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+# construct the virtual root and script directories.
 mkdir -p root/usr/local/SystemVPN_AD
 mkdir -p root/Library/LaunchDaemons
 mkdir scripts
+
+# copy the items into the appriate locations and create the postinstall script
+# to load the launch daemon
+
 cp vpnCheckConnect.py root/usr/local/SystemVPN_AD/
 cp edu.syr.ad.vpnconnect.plist root/Library/LaunchDaemons/
 echo "#!/bin/bash\nlaunchctl load -w /Library/LaunchDaemons/edu.syr.vpnconnect.plist\nexit 0" > scripts/postinstall
+
+# ensure execution bits are set
 chmod +x root/usr/local/SystemVPN_AD
 chmod +x scripts/postinstall
-pkgbuild --root ./root --scripts ./scripts --identifier edu.syr.ad.vpnconnect --version 1.0 --filter "\.DS_Store" vpnCheckConnect-1.0.pkg
 
+# create the install package
+pkgbuild --root ./root \
+--scripts ./scripts \
+--identifier edu.syr.ad.vpnconnect \
+--version 1.0 \
+--filter "\.DS_Store" \
+vpnCheckConnect.pkg
+
+# clean up yer stuff.
 rm -Rf ./root
 rm -Rf ./scripts
 
